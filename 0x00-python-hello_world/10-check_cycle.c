e8ce4b87d12e176baff89fd8e2d1dc0ba33d0cea
Nadira3 <paitanun35@gmail.com>
2023-06-05 19:23:35 -0700

    reverted changes to correct errors

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,26 +7,16 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *ptr = NULL;
-	listint_t *prev = NULL;
-	listint_t *new = NULL;
-	int flag = 0;
 
 	ptr = list;
-	prev = list;
-	new = list->next;
 	if (list)
 	{
 		while (ptr->next)
 		{
-			if (((ptr == list) && flag) || ((ptr == prev) && flag) || ptr == new)
+			if (ptr->next == list)
 				return (1);
 			ptr = ptr->next;
-			if (flag)
-				prev = prev->next;
-			new = new->next;
-			flag++;
 		}
-		return (0);
 	}
 	return (0);
 }
