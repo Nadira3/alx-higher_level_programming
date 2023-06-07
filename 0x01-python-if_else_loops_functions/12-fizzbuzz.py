690c92ea8ae8effb72b8ef3137efa5659cbe9c7d
Nadira3 <paitanun35@gmail.com>
2023-06-06 19:41:52 -0700

    12-fizzbuzz.py first submission

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        end = "\n" if i > 98 else " "
+        if i % 3 == 0:
+            print("Fizz" if i % 5 != 0 else "FizzBuzz", end=end)
+        elif i % 5 == 0:
+            print("Buzz", end=end)
+        else:
+            print("{:d}".format(i), end=end)
