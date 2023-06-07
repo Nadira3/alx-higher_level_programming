13980a6344ab12dd796fb1237b553424faa27136
Nadira3 <paitanun35@gmail.com>
2023-06-06 19:01:50 -0700

    implemented empty string argument

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     n = len(str)
-    for i in range(n):
+    for i in range(n if n > 0 else 1):
         p = ord(str[i]) - 32 if 97 <= ord(str[i]) <= 122 else ord(str[i])
         end = "\n" if i == n - 1 else ""
         print("{:c}".format(p), end=end)
