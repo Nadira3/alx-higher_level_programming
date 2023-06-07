2174da071e5856e0a06e3bb0783a01a3f3167b12
Nadira3 <paitanun35@gmail.com>
2023-06-06 17:26:20 -0700

    fixed error: You can only use no more than 2 print functions with string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("{:d}".format(0), end="")
-    print("{}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
-    else:
-        print()
+    print("{:02d}".format(i) if i < 10 else "{:d}".format(i), end="")
+    print(", " if i < 99 else '\n', end="") 
