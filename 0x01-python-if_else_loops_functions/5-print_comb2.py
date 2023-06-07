ef6ca44a69903e9f21c1b6b0ce55c0d2d48098b6
Nadira3 <paitanun35@gmail.com>
2023-06-06 17:29:50 -0700

    fixed error: 5-print_comb2.py:4:44: W291 trailing whitespace

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
     print("{:02d}".format(i) if i < 10 else "{:d}".format(i), end="")
-    print(", " if i < 99 else '\n', end="") 
+    print(", " if i < 99 else '\n', end="")
