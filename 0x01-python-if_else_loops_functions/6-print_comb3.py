0cb914bbdfbd423162ab1d7a2770824e5c6754d2
Nadira3 <paitanun35@gmail.com>
2023-06-06 17:51:08 -0700

    fixed syntax error in if statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i > int((str(i))[::-1])
+    if i > int((str(i))[::-1]):
         continue
+    if i > 9:
+        if str(i)[0] == str(i)[1]:
+            continue
     print("{:02d}".format(i) if i < 10 else "{:d}".format(i), end="")
-    print(", " if i < 99 else '\n', end="")
+    print(", " if i < 89 else '\n', end="")
