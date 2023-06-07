01b8c2017bb2821b4a85fb9f008f96013d42aab6
Nadira3 <paitanun35@gmail.com>
2023-06-06 17:15:13 -0700

    5-print_comb2.py first submission

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        print("{:d}".format(0), end="")
+    print("{}".format(i), end="")
+    if i < 99:
+        print(", ", end="")
+    else:
+        print()
