ad3a0bc68f092e3b0734a36196f42fc989e1e12f
Nadira3 <paitanun35@gmail.com>
2023-06-06 19:13:38 -0700

    fixed empty string error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,9 @@
 def uppercase(str):
     n = len(str)
     for i in range(n if n > 0 else 1):
-        p = ord(str[i]) - 32 if 97 <= ord(str[i]) <= 122 else ord(str[i])
+        if n == 0:
+            p = 10
+        else:
+            p = ord(str[i]) - 32 if 97 <= ord(str[i]) <= 122 else ord(str[i])
         end = "\n" if i == n - 1 else ""
         print("{:c}".format(p), end=end)
