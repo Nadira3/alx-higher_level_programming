7ba045f87fae1cf40d22a3167c9c5e87c9f9aaa8
Nadira3 <paitanun35@gmail.com>
2023-06-08 04:49:17 -0700

    improved code for efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,14 +6,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr = NULL;
+	listint_t *ptr = NULL, *ptr2 = NULL;
 
 	ptr = list;
+	ptr2 = list;
 	if (list)
 	{
-		while (ptr->next)
+		while (ptr->next && ptr2)
 		{
-			if (ptr->next == list)
+			ptr2 = ptr->next->next;
+			if (ptr->next == list || ptr->next == ptr2)
 				return (1);
 			ptr = ptr->next;
 		}
