388d188d1467e84063de4b3aa55a7d9fa1b9474e
Nadira3 <paitanun35@gmail.com>
2023-06-08 07:05:43 -0700

    correction to improve efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,43 @@
 #include "lists.h"
-/**
- * iscycle - checks if a list is a circular list
- * @node: node to be compared in the list
- * @n: position of node in the list
- * @list: list
- * Return: 0 || 1
+/*
+ *
+ * int iscycle(listint_t *node, int n, listint_t *list)
+ * {
+ *	listint_t *ptr = NULL;
+ *	int i = 0;
+ *
+ *	ptr = list;
+ *	while (i < n && ptr->next)
+ *	{
+ *		i++;
+ *		ptr = ptr->next;
+ *		if (ptr->next == node->next && i < n && ptr != list)
+ *			return (1);
+ *	}
+ *	return (0);
+ *  } this was my previous solution. it works but its very slow. youll see this later and laugh. lol
+ *  anyway's i had to learn the floyd's algorithm to solve this task.
+ * int check_cycle(listint_t *list)
+ * {
+ *	listint_t *ptr = NULL;
+ *	int i = 0;
+ *
+ *	ptr = list;
+ *	if (list)
+ *	{
+ *		while (ptr->next)
+ *		{
+ *			if (ptr->next == list || iscycle(ptr, i, list))
+ *				return (1);
+ *			ptr = ptr->next;
+ *			i++;
+ *		}
+ *	}
+ *	return (0);
+ * }
  */
-int iscycle(listint_t *node, int n, listint_t *list)
-{
-	listint_t *ptr = NULL;
-	int i = 0;
 
-	ptr = list;
-	while (i < n && ptr->next)
-	{
-		i++;
-		ptr = ptr->next;
-		if (ptr->next == node->next && i < n && ptr != list)
-			return (1);
-	}
-	return (0);
-}
+
 /**
  * check_cycle - checks if a list is a circular list
  * @list: list
@@ -28,18 +45,19 @@ int iscycle(listint_t *node, int n, listint_t *list)
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *ptr = NULL;
+	listint_t *ptr, *ptr2;
 	int i = 0;
 
 	ptr = list;
+	ptr2 = list;
 	if (list)
 	{
-		while (ptr->next)
+		while (ptr2 && ptr2->next)
 		{
-			if (ptr->next == list || iscycle(ptr, i, list))
-				return (1);
 			ptr = ptr->next;
-			i++;
+			ptr2 = ptr2->next->next;
+			if (ptr == ptr2)
+				return (1);
 		}
 	}
 	return (0);
