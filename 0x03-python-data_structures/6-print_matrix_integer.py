d1a96e3a3822a9c707d4cd1253715025a29d8389
Nadira3 <paitanun35@gmail.com>
2023-06-12 05:08:54 +0000

    removed condition to check if matrix is present

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            top = i
-            for j in top:
-                print("{:d}".format(j), end=" " if j != top[-1] else '\n')
+    for i in matrix:
+        top = i
+        for j in top:
+            print("{:d}".format(j), end=" " if j != top[-1] else '\n')
