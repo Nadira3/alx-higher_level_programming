1e0f07330eb87d77e623e3f593bcf573656efcc9
Nadira3 <paitanun35@gmail.com>
2023-06-12 09:24:17 +0000

    fixed pycodestyle error: 6-print_matrix_integer.py:9:17: E117 over-indented

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
         if not top:
             print()
         for j in top:
-                print("{:d}".format(j), end=" " if j != top[-1] else '\n')
+            print("{:d}".format(j), end=" " if j != top[-1] else '\n')
