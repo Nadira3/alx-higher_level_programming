1a24d6c3a41284bb239f3a2c88c8035891c543ba
Nadira3 <paitanun35@gmail.com>
2023-06-12 09:22:46 +0000

    added condition to check for empty list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         top = i
+        if not top:
+            print()
         for j in top:
-            print("{:d}".format(j), end=" " if j != top[-1] else '\n')
+                print("{:d}".format(j), end=" " if j != top[-1] else '\n')
