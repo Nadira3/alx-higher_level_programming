aa85676850c16e6850e9a7f0f205c1a36de91719
Nadira3 <paitanun35@gmail.com>
2023-06-13 06:33:56 +0000

    fixed ternary operator error

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -21,12 +21,13 @@ int is_palindrome(listint_t **head)
 	}
 	ptr = *head;
 	len = list_len;
-	for (i = 0; i < list_len % 2 ? (list_len / 2) - 1 : list_len / 2; i++)
+	for (i = 0; i < (list_len % 2 == 0 ? list_len / 2 : list_len / 2 + 1); i++)
 	{
 		while (j++ < len - 1)
 			pal = pal->next;
 		if (ptr->n != pal->n)
 			return (0);
+		printf("i: %d, list_len: %d, ptr->n: %d, pal->n: %d\n", i, list_len / 2, ptr->n, pal->n);
 		len--;
 		ptr = ptr->next;
 		pal = *head;
