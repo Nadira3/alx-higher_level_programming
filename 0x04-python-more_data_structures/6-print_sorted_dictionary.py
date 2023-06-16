7f51fa539e5cc6da2463f6153fd67fb7f39436fa
Nadira3 <paitanun35@gmail.com>
2023-06-16 00:33:06 +0000

    added condition to handle None object

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i, j in sorted(a_dictionary.items()):
-        print(i, j)
+    if a_dictionary:
+        for i, j in sorted(a_dictionary.items()):
+            print(i, j)
+#a_dictionary = None
+#print_sorted_dictionary(a_dictionary)
