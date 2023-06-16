ecce6980a73bde875e66f3c3fe13b31c73735147
Nadira3 <paitanun35@gmail.com>
2023-06-16 00:37:43 +0000

    fixed style error: 6-print_sorted_dictionary.py:6:1: E265 block comment should start with '# '

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,3 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         for i, j in sorted(a_dictionary.items()):
             print("{:s}: {}".format(i, j))
-#a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-#print_sorted_dictionary(a_dictionary)
