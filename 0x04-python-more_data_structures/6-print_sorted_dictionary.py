12751feaea6f6f4b9ef87eda4145f5c2edfe06fc
Nadira3 <paitanun35@gmail.com>
2023-06-16 00:36:18 +0000

    included format specifier and format string

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         for i, j in sorted(a_dictionary.items()):
-            print(i, j)
-#a_dictionary = None
+            print("{:s}: {}".format(i, j))
+#a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
 #print_sorted_dictionary(a_dictionary)
