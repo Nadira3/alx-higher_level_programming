c4e931329116cb2c61f910ca38a47725fa819e8b
Nadira3 <paitanun35@gmail.com>
2023-06-16 04:21:05 +0000

    handled subtraction of numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,29 +4,17 @@ def roman_to_int(roman_string):
     idx = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 
     total = 0
-    flag = 0
     if roman_string and isinstance(roman_string, str):
-        for i in roman_string:
+        rlen = len(roman_string)
+        for i in range(rlen):
             for j in range(len(idx)):
-                if i == idx[j]:
+                if roman_string[i] == idx[j]:
                     value = int(num[j])
-                    flag = j
-                    total += value
+                    if i < rlen - 1 and \
+                            idx.index('{}'.format(roman_string[i + 1])) > j:
+                        total -= value
+                    else:
+                        total += value
         return total
     else:
         return 0
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
