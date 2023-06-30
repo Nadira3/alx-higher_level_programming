aa3574932968003ca2f8aa2e3af02b21228de68d
Nadira3 <paitanun35@gmail.com>
2023-06-30 12:20:16 +0100

    magic calculation correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    if (a < b):
+
+    if a < b:
         c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
     else:
         c = sub(a, b)
-
-
-for i in range(4, 6):
-    return (add(c, i))
-    c = 4
-dis.dis(magic_calculation)
+    
+    return c
