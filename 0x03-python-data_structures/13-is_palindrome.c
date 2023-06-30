43115e70970cd3c517e4d84bbc3a4275f94869f7
Nadira3 <paitanun35@gmail.com>
2023-06-30 15:52:21 +0100

    is palindrome correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,8 +6,9 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *pam, *pal;
-	int list_len = 0, i, j = 0, len;
+	listint_t *pam = NULL, *pal = NULL, *f;
+	listint_t *ptr1 = NULL, *ptr2 = NULL, *ptr3 = NULL;
+	int len = 0;
 
 	if (!head)
 		return (0);
@@ -19,11 +20,46 @@ int is_palindrome(listint_t **head)
 		if (!(pal->next->next))
 		{
 			pal = pal->next;
+			len += 1;
 			break;
 		}
 		pam = pam->next;
 		pal = pal->next->next;
+		len += 2;
 	}
+	len += *head ? 1 : 0;
 	/* at this point pam is at the middle of the list */
-	printf("pal->n: %d, pam->next: %d\n", pal->n, pam->n);
+	if (len)
+	{
+		pal = *head;
+		ptr1 = pam;
+		ptr2 = pam->next;
+		while (ptr2)
+		{
+			ptr3 = ptr2->next;
+			ptr2->next = ptr1;
+			ptr1 = ptr2;
+			ptr2 = ptr3;
+		}
+		ptr2 = ptr1;
+		pam->next = NULL;
+		while (len % 2? pal->next : pal)
+		{
+			if (ptr1->n != pal->n)
+			{
+				while (ptr1->next)
+				{
+					f = ptr1;
+					ptr1 = ptr1->next;
+					free(f);
+				}
+				return (0);
+			}
+			ptr1 = ptr1->next;
+			free(ptr2);
+			ptr2 = ptr1;
+			pal = pal->next;
+		}
+	}
+	return (1);
 }
