f98d4984ea978668a286f1da99d989814c41da9b
Nadira3 <paitanun35@gmail.com>
2023-07-03 06:04:18 +0100

    rearranged condition in position setter to prevent out od range error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,9 +36,9 @@ class Square:
     def position(self, position):
         if not isinstance(position, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(position[0], int) or\
-                not isinstance(position[1], int) or\
-                len(position) != 2:
+        elif len(position) != 2 or\
+                not isinstance(position[0], int) or\
+                not isinstance(position[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
