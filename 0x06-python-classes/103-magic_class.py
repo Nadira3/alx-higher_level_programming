ede4ef9c9cdf225aa7d0d717d1d2f75c2dbabbaf
Nadira3 <paitanun35@gmail.com>
2023-07-03 19:14:58 +0100

    defined a maths class to return pi to magicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 """
+    math: returns the mathematical constant
+    of a circle, π
+"""
+
+
+class math:
+    """
+        math: returns the mathematical constant
+        of a circle, π
+    """
+    def pi():
+        return 3.141592653589793
+
+
+"""
     MagicClass: finds the area and circumference
     of a circle
 """
@@ -16,7 +31,7 @@ class MagicClass:
             raise TypeError("radius must be a number")
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi()
 
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        return 2 * math.pi() * self.__radius
