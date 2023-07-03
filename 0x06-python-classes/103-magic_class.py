66fc30a059e96152fe0bd337694b8c83198d83d7
Nadira3 <paitanun35@gmail.com>
2023-07-03 19:07:17 +0100

    fixed pycodestyle error and removed none assignament to radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import math
 """
     MagicClass: finds the area and circumference
     of a circle
 """
+
+
 class MagicClass:
     """
         MagicClass: finds the area and circumference
@@ -11,9 +14,7 @@ class MagicClass:
     def __init__(self, radius=0):
         self.__radius = radius
         if type(radius) is not int and type(radius) is not float:
-             raise TypeError("radius must be a number")
-        else:
-            self.__radius = None
+            raise TypeError("radius must be a number")
 
     def area(self):
         return self.__radius ** 2 * math.pi
