90fdab44cef8066203c08f9e1aa8c223c7bd535f
Nadira3 <paitanun35@gmail.com>
2023-07-03 03:44:08 +0100

    task 3 first submission

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Square: defines a square"""
+
+
+class Square:
+
+    """
+        Square: a class Square that defines a square
+
+        size: to compute the area of the square
+        many others
+    """
+
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+    
+    def area(self):
+        return self.size ** 2
