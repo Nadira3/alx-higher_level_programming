0d9744d7dcbe70c90d41628a540605008dd21054
Nadira3 <paitanun35@gmail.com>
2023-07-03 03:15:09 +0100

    task 1 first submission

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""defines a square"""
+
+
+class Square:
+
+    """
+        a class Square that defines a square
+
+        size: to compute the area of the square
+        many others
+    """
+
+    def __init__(self, size):
+        self._size = size
