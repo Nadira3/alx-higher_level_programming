74f5ae6c46ac0022e8852aa185880ea28a9cbb80
Nadira3 <paitanun35@gmail.com>
2023-07-03 03:09:58 +0100

    fixed pycodestyle error: 0-square.py:4:1: E302 expected 2 blank lines, found 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """defines a square:"""
+
+
 class Square:
-	"""an empty class Square that defines a square"""
-	...
+    """an empty class Square that defines a square"""
+    ...
