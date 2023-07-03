d7ab5216b26bbcd8af15068d5d29a3d1a71b47b0
Nadira3 <paitanun35@gmail.com>
2023-07-03 18:51:07 +0100

    >added module and class documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    MagicClass: finds the area and circumference
+    of a circle
+"""
 class MagicClass:
+    """
+        MagicClass: finds the area and circumference
+        of a circle
+    """
     def __init__(self, radius=0):
         self.__radius = radius
         if type(radius) is not int and type(radius) is not float:
