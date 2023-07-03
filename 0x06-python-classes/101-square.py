5d67c62eaf5b1d060c7f088c49799e2c9b642da4
Nadira3 <paitanun35@gmail.com>
2023-07-03 17:57:41 +0100

    str function implementation for sqaure.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""Square: defines a square"""
+
+
+class Square:
+
+    """
+        Square: a class Square that defines a square
+
+        size: to compute the area of the square
+        many others
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) != 2 or\
+                not isinstance(position[0], int) or\
+                not isinstance(position[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+
+    def __str__(self):
+        if self.__position[1] > 0 and self.size:
+            for count in range(self.__position[1]):
+                print()
+        for count in range(self.__size):
+            end = "\n" if count != self.__size - 1 else ""
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size, end=end)
+        return ""
+
+    """
+        area: computes the area of a square
+
+        Return: area of a square of size 'size'
+    """
+    def area(self):
+        return self.__size ** 2
+
+    """
+        my_print: prints a square
+    """
+    def my_print(self):
+        if self.__position[1] > 0 and self.size:
+            for count in range(self.__position[1]):
+                print()
+        for count in range(self.__size):
+            end = "\n" if count != self.__size - 1 else ""
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size, end=end)
+        print()
