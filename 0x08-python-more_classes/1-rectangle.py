0ba34cb1f8d24458d12e0f81fe973fa16948278e
Nadira3 <paitanun35@gmail.com>
2023-07-04 03:52:57 +0100

    instantiation of Rectangle class with two private attributes, width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Rectangle: defines a rectangle"""
+
+
+class Rectangle:
+
+    """
+        Rectangle: a class Rectangle that defines a square
+
+        width: defines a width
+        height: defines a height
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__size = width
+        self.__height = height
+
+    """ getter for width """
+    @property
+    def width(self):
+        return self.__width
+    
+    """ setter for width """
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+    
+    """ getter for height """
+    @property
+    def height(self):
+        return self.__height
+
+    """ setter for height """
+    def height(self):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
