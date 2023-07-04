2aec7719211e6e11357ab298a5a6c3001035faff
Nadira3 <paitanun35@gmail.com>
2023-07-04 04:38:49 +0100

    __str__ method implementation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""Rectangle: defines a rectangle"""
+
+
+class Rectangle:
+
+    """
+        Rectangle: a class Rectangle that defines a square
+
+        width: defines a width
+        height: defines a height
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ getter for width """
+
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ setter for width """
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """ getter for height """
+
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ setter for height """
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        for height in range(self.__height):
+            end = "" if height == self.__height - 1 else "\n"
+            print("#" * self.__width, end=end)
+        return ""
