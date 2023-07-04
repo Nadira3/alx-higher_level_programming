5e6141ce55c59ba67fbb46e066467f9749fae42d
Nadira3 <paitanun35@gmail.com>
2023-07-04 05:22:38 +0100

    magic calculation byte code task

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+import dis
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if a > i:
+                raise Exception("Too far")
+            else:
+                result += a ** b / i
+        except:
+            result = a + b
+    return result
+
+dis.dis(magic_calculation)
