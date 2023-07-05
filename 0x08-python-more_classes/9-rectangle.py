93d129654ffa9200a74ef05e65963406d8859bfa
Nadira3 <paitanun35@gmail.com>
2023-07-05 15:26:44 +0100

    tested peer method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -114,4 +114,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        width = size
+        height = size
+        return Rectangle(width, height)
