f51fc8f75944f34d092a377f8829071a14425d78
Nadira3 <paitanun35@gmail.com>
2023-07-05 14:08:27 +0100

    reversed err correction

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -114,9 +114,4 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        if not isinstance(size, int):
-            raise TypeError("width must be an integer")
-        elif size < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            return cls(size, size)
+        return cls(size, size)
