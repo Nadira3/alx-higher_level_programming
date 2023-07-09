48318f5c7ba8b1e986b72fed7f425630979fea52
Nadira3 <paitanun35@gmail.com>
2023-07-09 15:18:08 +0100

    pycodestyle error fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,9 +13,12 @@ def matrix_divided(matrix, div):
 
         Returns: a new matrix
     """
-
-    if not isinstance(matrix, list) or any(not isinstance(li, list) for li in matrix) or any(not isinstance(num, (int, float)) for li in matrix for num in li):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    var = matrix
+    err = "matrix must be a matrix (list of lists) of integers/floats"
+    if not isinstance(var, list) or \
+            any(not isinstance(li, list) for li in var) \
+            or any(not isinstance(n, (int, float)) for li in var for n in li):
+        raise TypeError(err)
     if any(len(matrix[0]) != len(li) for li in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
