4daa58104a0b5311b3d65adc85b244057901faff
Nadira3 <paitanun35@gmail.com>
2023-07-09 19:09:02 +0100

    added test for none argument

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """
         TestMaxInteger - unittests for the function
@@ -17,13 +18,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_default(self):
         self.assertEqual(max_integer(), None)
 
+    def test_noarg(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
     def test_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
         self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
         self.assertEqual(max_integer([1, 2, 3, -2, -1]), 3)
         self.assertEqual(max_integer([-1, 0, -3, -2, -1]), 0)
         self.assertEqual(max_integer([-1, -6, -3, -2, -4]), -1)
-    
+ 
     def test_other(self):
         with self.assertRaises(TypeError):
             max_integer("string")
