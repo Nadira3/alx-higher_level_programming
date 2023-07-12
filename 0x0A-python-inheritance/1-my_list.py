d7f9b83a9b2d2bc64ca8ba5f520479d71176c95d
Nadira3 <paitanun35@gmail.com>
2023-07-12 09:23:30 +0100

    removed init method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,9 +11,6 @@ class MyList(list):
         inherits attributes from a class
     """
 
-    def __init__(self):
-        list.__init__(self)
-
     def print_sorted(self):
         """
             prints a sorted list
