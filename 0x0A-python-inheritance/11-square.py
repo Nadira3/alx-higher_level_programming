802a00ede70f5e8e59055382c202f34c8c986ab5
Nadira3 <paitanun35@gmail.com>
2023-07-12 23:49:48 +0100

    print and area implementation

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    Square module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        inherits attributes from BaseGeometry
+    """
+
+    def __init__(self, size):
+        """
+            init method for Rectangle class
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
