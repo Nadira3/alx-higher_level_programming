680a370a20fd4c65bc803cd36622ebeb65c72dd6
Nadira3 <paitanun35@gmail.com>
2023-07-13 20:11:04 +0100

    task 0 and 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    read_file module
+"""
+
+
+def read_file(filename=""):
+    """
+        reads a text file (UTF8)
+        and prints it to stdout
+    """
+    
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
+
+    print(read_file)
