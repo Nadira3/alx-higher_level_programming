437116d100fcc5b3285720873dcee07034bbea32
Nadira3 <paitanun35@gmail.com>
2023-07-15 09:39:55 +0100

    removed whitespace fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
     """
         returns a triangle of numbers
     """
-    
+
     result = []
 
     if n > 0:
@@ -21,11 +21,11 @@ def pascal_triangle(n):
                     if len(result[i]) == len(result[i - 1]):
                         result[i].append(1)
                         break
-                    
+
                     if i == 1:
                         result[i].append(1)
                         continue
 
                     result[i].append(result[i - 1][j] + result[i - 1][j + 1])
-    
+
     return result
