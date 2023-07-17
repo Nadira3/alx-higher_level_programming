185d865ab944194daf1e9ae2aa7c6ade64c3d2f3
Nadira3 <paitanun35@gmail.com>
2023-07-17 19:42:04 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,6 +28,12 @@ class Rectangle(Base):
     def width(self, width):
         """ setter function for width """
 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = width
 
     @property
@@ -39,6 +45,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """ setter function for width """
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+
+        if height <= 0:
+            raise ValueError("height must be > 0")
 
         self.__height = height
 
@@ -52,6 +64,12 @@ class Rectangle(Base):
     def x(self, x):
         """ setter function for variable x """
 
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = x
 
     @property
@@ -64,4 +82,38 @@ class Rectangle(Base):
     def y(self, y):
         """ setter unction for variable x """
 
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = y
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        num = self.y
+        for count in range(self.height + num):
+            if num:
+                print()
+                num -= 1
+                continue
+            if (self.x):
+                print(" " * self.x)
+            print("#" * self.width)
+    
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        if not args:
+            for key, value in kwargs:
+                self.f"{key}" = value
+        else:
+           attr = ['id', 'width', 'height', 'x', 'y']
+           i = 0
+           for arg in args:
+               self.f"{attr[i]}" = arg
+               i += 1
