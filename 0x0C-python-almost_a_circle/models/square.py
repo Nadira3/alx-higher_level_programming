12e547198c38ee3c3a35d72b224dd128daba3d9e
Nadira3 <paitanun35@gmail.com>
2023-07-18 03:46:50 +0100

    added size attribute to square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,8 +16,27 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """ print implementation in class """
 
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ getter function for size """
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ setter function for size """
+
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+
+        if size <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__size = size
