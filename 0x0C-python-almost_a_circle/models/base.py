630a13cf1905a6d3dbc1440c8f459bf009788f2f
Nadira3 <paitanun35@gmail.com>
2023-07-18 05:33:38 +0100

    more json implementations

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
             returns the JSON string representation
@@ -29,3 +30,29 @@ class Base:
         """
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        flist = ["Rectangle", "Square"]
+        if list_objs is None:
+            for arg in flist:
+                file = arg + ".json"
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string([]))
+        else:
+            for arg in list_objs:
+                file = str(arg.__class__.__name__) + ".json"
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+            returns the JSON string representation
+            of list_dictionaries
+        """
+
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
