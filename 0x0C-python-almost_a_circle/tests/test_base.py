48f8810e6cc73365ab4f8d4533a7b590ad5a375e
Nadira3 <paitanun35@gmail.com>
2023-07-20 18:38:37 +0100

    test files upload

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    base test module
+"""
+
+
+import unittest
+import math
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+
+    """
+        TestBase - unittests for the function
+    """
+
+    def test_id(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
+        b = Base("str")
+        self.assertEqual(b.id, 'str')
+        b = Base(float("inf"))
+        self.assertEqual(b.id, float("inf"))
+        b = Base([1, 2, 3])
+        self.assertEqual(b.id, [1, 2, 3])
+        b = Base((1, 2, 3))
+        self.assertEqual(b.id, (1, 2, 3))
+        b = Base({"one": 1, "two": 2, "three": 3})
+        self.assertEqual(b.id, {'one': 1, 'two': 2, 'three': 3})
+        b = Base(0.5)
+        self.assertEqual(b.id, 0.5)
+        b = Base(-5)
+        self.assertEqual(b.id, -5)
+        b = Base(float("NaN"))
+        self.assertTrue(math.isnan(b.id))
