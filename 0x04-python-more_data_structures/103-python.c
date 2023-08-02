6fb00d73add5acaf36730fe221c0e77d61a2a046
Nadira3 <paitanun35@gmail.com>
2023-08-02 16:14:34 +0100

    pthon bytes task update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,32 +1,6 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - Prints basic information about a Python list
- * @p: The Python list to analyze
- *
- * Return: None
- */
-void print_python_list_info(PyObject *p)
-{
-	Py_ssize_t i, n;
-	PyObject *item;
-	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
-
-	n = PyList_Size(p);
-	if (n < 0)
-		return;
-	printf("[*] Size of the Python List = %ld\n", n);
-	printf("[*] Allocated = %ld\n", allocated);
-	for (i = 0; i < n; i++)
-	{
-		item = PyList_GetItem(p, i);
-		if (!item)
-			break;
-		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
-	}
-}
-
-/**
  * print_python_bytes - Prints basic information about a Python object
  * @p: The Python object to analyze
  *
@@ -36,11 +10,52 @@ void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t i, n;
 	PyBytesObject *bits;
+	char *ptr = NULL;
 
 	printf("[.] bytes object info\n");
 	bits = (PyBytesObject*)p;
 	if (PyBytes_Check(bits))
-		printf("  size: %ld\n", bits->PyObject_VAR_HEAD.ob_size);
+	{
+		n = bits->ob_base.ob_size;
+		ptr = bits->ob_sval;
+		printf("  size: %ld\n", n);
+		ptr[n] = 0;
+		printf("  trying string: %s\n", ptr);
+		printf("  first %ld bytes:", n < 10 ? n + 1 : 10);
+		for (i = 0; i < (n < 10 ? n + 1 : 10); i++, ptr++)
+			printf(" %.2x", *ptr);
+		puts("");
+
+	}
 	else
 		printf("  [ERROR] Invalid Bytes Object\n");
+}
+
+/**
+ * print_python_list_info - Prints basic information about a Python list
+ * @p: The Python list to analyze
+ *
+ * Return: None
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i, n;
+	PyObject *item;
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+	n = PyList_Size(p);
+	if (n < 0)
+		return;
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", n);
+	printf("[*] Allocated = %ld\n", allocated);
+	for (i = 0; i < n; i++)
+	{
+		item = PyList_GetItem(p, i);
+		if (!item)
+			break;
+		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+	}
 }
