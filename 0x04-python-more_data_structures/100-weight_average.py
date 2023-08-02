ae2d01efb03953c553bea97a6e3782bcc2545dd3
Nadira3 <paitanun35@gmail.com>
2023-08-02 12:22:50 +0100

    empty list check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,9 @@
 def weight_average(my_list=[]):
     total = 0
     num = 0
-    for arg in my_list:
-        product = arg[0] * arg[1]
-        total += product
-        num += arg[1]
+    if len(my_list) > 0:
+        for arg in my_list:
+            product = arg[0] * arg[1]
+            total += product
+            num += arg[1]
     return total / num
