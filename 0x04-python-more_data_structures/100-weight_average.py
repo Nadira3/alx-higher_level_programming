6e0f22bd308089e4f9d20d2e5856902237a799c1
Nadira3 <paitanun35@gmail.com>
2023-08-02 12:26:53 +0100

    zero division fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,13 @@
 def weight_average(my_list=[]):
     total = 0
     num = 0
-    if len(my_list) > 0:
+    if my_list:
         for arg in my_list:
             product = arg[0] * arg[1]
             total += product
             num += arg[1]
-    return total / num
+    return total / num if my_list else 0
+
+list = []
+result = weight_average(list)
+print("Av: {:0.2f}".format(result))
