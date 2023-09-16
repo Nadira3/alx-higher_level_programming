627e6cd5f3f40db74cb9bb13911569bf42eb0cc4
Nadira3 <paitanun35@gmail.com>
2023-09-16 18:29:37 +0100

    properly handled exception errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,22 +2,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    type_list = [str, list, dict, set, tuple, float]
     try:
         print("{:d}".format(value))
         return True
-    except:
-        for item in range(len(type_list) - 1):
-            if isinstance(value, type_list[item]):
-                value_type = str(type_list[item])
-                value = ""
-                i = 0
-                while value_type[i] != '\'':
-                    i += 1
-                while value_type[i] != '>':
-                    value += value_type[i]
-                    i += 1
-
-        print("Exception: Unknown format code 'd' for object of type {}".format(value),file=sys.stderr)
+    except Exception as err:
+        print(err, file=sys.stderr)
         return False
-
