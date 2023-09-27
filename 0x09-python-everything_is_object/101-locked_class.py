62f43b8cc154106429c7eec33441b0998dafc374
Nadira3 <paitanun35@gmail.com>
2023-09-27 19:00:38 +0100

    lockedClass proper implementation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,13 +8,4 @@ class LockedClass:
     """ Regulates the names of attributes
         an instance initializes
     """
-
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
-        super().__setattr__(name, value)
-
-    def __getattr__(self, name):
-        if name not in self.__dict__:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
-        super().__getattr__(name)
+    __slots__ = ("first_name")
