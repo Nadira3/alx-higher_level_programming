e769cbe4c87a4fc01c6808bec696b2b2f3e6632c
Nadira3 <paitanun35@gmail.com>
2023-09-27 13:18:59 +0100

    fixed pycodestyle error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-magic_class = __import__('101-magic_class').magic_class
 def magic_string():
+    magic_class = __import__('101-magic_class').magic_class
     return magic_class()
