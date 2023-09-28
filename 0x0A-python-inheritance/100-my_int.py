039990485d82feebc244bcfd4a8c125930a9e42d
Nadira3 <paitanun35@gmail.com>
2023-09-28 05:54:42 +0100

    task 12 solution

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    Int Inheritance module
+"""
+
+
+class MyInt(int):
+    """
+        MyInt has == and != operators inverted
+    """
+    
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+    
+    def __eq__(self, other):
+        return self.value != other
+
+    def __ne__(self, other):
+        return self.value == other
