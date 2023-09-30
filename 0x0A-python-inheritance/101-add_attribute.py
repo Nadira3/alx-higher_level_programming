ab9785250b0fe0959711830ecf4fb66f4dc9b41e
Nadira3 <paitanun35@gmail.com>
2023-09-30 21:24:46 +0100

    module and function documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+""" 
+    module containing function
+    typechecking __setattr__
+"""
+    
 
 def add_attribute(obj, key, value):
+    """
+        __setattr__ with error typecheck
+    """
+
     if isinstance(obj, (float, int, str, list, dict, tuple)):
         raise TypeError("can't add new attribute")
     else:
