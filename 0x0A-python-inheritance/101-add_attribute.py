c0918ff4dbe1f606cd1c0186799d2fc9cea71fdd
Nadira3 <paitanun35@gmail.com>
2023-10-03 06:08:09 +0100

    correction for setattr function

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" 
+"""
     module containing function
     typechecking __setattr__
 """
-    
+
 
 def add_attribute(obj, key, value):
     """
         __setattr__ with error typecheck
     """
 
-    if isinstance(obj, (float, int, str, list, dict, tuple)):
+    if not isinstance(obj, object) or key not in obj.__slots__:
         raise TypeError("can't add new attribute")
     else:
         obj.__setattr__(key, value)
