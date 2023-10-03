7d5b85ede581acd40406330cce177f3bdc23df04
Nadira3 <paitanun35@gmail.com>
2023-10-03 07:19:19 +0100

    third correction

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -10,7 +10,9 @@ def add_attribute(obj, key, value):
         __setattr__ with error typecheck
     """
 
-    if obj.__class__.__name__ in ("float", "dict", "int", "str", "tuple", "list")\
-            or hasattr(obj, "__slots__") and key not in obj.__class__.__slots__:
+    if obj.__class__.__name__ in\
+            ("float", "dict", "int", "str", "tuple", "list")\
+            or hasattr(obj, "__slots__")\
+            and key not in obj.__class__.__slots__:
         raise TypeError("can't add new attribute")
     obj.__setattr__(key, value)
