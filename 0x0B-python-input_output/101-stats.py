7dddfb79340f01673eea7e0c4228c4c157df4ac2
Nadira3 <paitanun35@gmail.com>
2023-10-03 21:08:25 +0100

    proper signal handling

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
 """ Log parse module """
 
-if __name__ == "__main__"
-import sys
-import signal
+if __name__ == "__main__":
+    import sys
+    import signal
 
-count = 0
-total_size = 0
-obj = {}
-statusCode = ""
+    count = 0
+    total_size = 0
+    obj = {}
+    statusCode = ""
 
-def handle_print():
-    """ module documentation """
-    print(f"File size: {total_size}")
-    for key, value in sorted(obj.items()):
-        print(f"{key}: {value}")
+    def handle_print():
+        """ module documentation """
+        print(f"File size: {total_size}")
+        for key, value in sorted(obj.items()):
+            print(f"{key}: {value}")
 
-def handle_interrupt(signal, frame):
-    """ module documentation """
-    handle_print()
-    sys.exit(1)
-
-signal.signal(signal.SIGINT, handle_interrupt)
-
-for line in sys.stdin:
-    if count >= 10 or line == "":
-        count = 0
+    def handle_interrupt(signal, frame):
+        """ module documentation """
         handle_print()
-        total_size = 0
-    try:
-        statusCode = line.split()[-2]
 
-        if statusCode in obj:
-            obj[statusCode] += 1
-        else:
-            obj[statusCode] = 1
+    signal.signal(signal.SIGINT, handle_interrupt)
 
-        count += 1
-        total_size += int(line.split()[-1])
-    except (ValueError, IndexError):
-        continue
+    for line in sys.stdin:
+        if count >= 10 or line == "":
+            count = 0
+            handle_print()
+            total_size = 0
+        try:
+            statusCode = line.split()[-2]
 
-help_print()
+            if statusCode in obj:
+                obj[statusCode] += 1
+            else:
+                obj[statusCode] = 1
+
+            count += 1
+            total_size += int(line.split()[-1])
+        except (ValueError, IndexError):
+            continue
+
+    handle_print()
