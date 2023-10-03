a3c25fdb90d3e7af68469b8919c4a9520083e613
Nadira3 <paitanun35@gmail.com>
2023-10-03 18:28:12 +0100

    log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+count = 0
+total_size = 0
+obj = {}
+statusCode = ""
+
+try:
+    for line in sys.stdin:
+        if count >= 10:
+            count = 0
+            print(f"File size: {total_size}")
+            for key, value in obj.items():
+                print(f"{key}: {value}")
+            total_size = 0
+        statusCode = line.split()[-2]
+
+        if statusCode in obj:
+            obj[statusCode] += 1
+        else:
+            obj[statusCode] = 1
+
+        count += 1
+        total_size += int(line.split()[-1])
+except KeyboardInterrupt:
+        print(f"File Size: {total_size}")
+        for key, value in obj.items():
+            print(f"{key}: {value}")
