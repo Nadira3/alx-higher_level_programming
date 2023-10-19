77387b80663f5c58e939b6d78be43651640d3a29
Nadira3 <paitanun35@gmail.com>
2023-10-19 12:19:05 +0100

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- creates a table in a database
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
+
+-- Inserts values into second_table
+INSERT INTO second_table (id, name, score)
+	VALUES (1, “John”, 10), (2, “Alex”, 3), (3, “Bob”, 14), (4, “George”, 8);
