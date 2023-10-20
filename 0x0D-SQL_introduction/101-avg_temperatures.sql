db74ed2c9ac07f56942c7cd5a09a999add97f334
Nadira3 <paitanun35@gmail.com>
2023-10-20 04:25:25 +0100

    correct import

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
 -- LOAD DATA INFILE "./temperatures.sql" INTO temperatures
-SOURCE temperatures.sql;
+
+
+
+--curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/temperatures.sql" -s | mysql -uroot -p hbtn_0c_0
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
