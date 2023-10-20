6f5e27294132df9eae774f2662245741b87860a7
Nadira3 <paitanun35@gmail.com>
2023-10-20 04:43:13 +0100

    removed import statement

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,2 @@
 -- LOAD DATA INFILE "./temperatures.sql" INTO temperatures
-SOURCE ./temperatures.sql;
-
-
-
-
 SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,3 @@
 
 -- LOAD DATA INFILE "./temperatures.sql" INTO temperatures
-SOURCE ./temperatures.sql;
-
-
 SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
