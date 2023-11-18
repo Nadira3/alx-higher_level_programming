d928d0a65c14b2abd0ac104e089abdf22775f446
Nadira3 <paitanun35@gmail.com>
2023-11-18 08:17:37 +0100

    (feat) => correction: You are not allowed to declare any new variable (var, let, etc..)

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
-class BaseObject {
-  constructor (base) {
-    this.base = base;
-  }
-}
-
 exports.converter = function (base) {
-  const baseObject = new BaseObject(base);
 
-  function convert (base) {
-    return base.toString(baseObject.base);
+  function convert (numBase) {
+    return numBase.toString(base);
   }
 
   return convert;
