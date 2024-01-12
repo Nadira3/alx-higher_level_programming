c67e223542488acef3d3815ef8ae1e1b0e24d476
Nadira3 <paitanun35@gmail.com>
2024-01-12 07:46:21 +0100

    tasl 0 correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; --AND 'user_0d_2'@'host';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'root'@'localhost'; --AND 'user_0d_2'@'host';
