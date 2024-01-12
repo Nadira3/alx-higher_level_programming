61afae7bc0621332a98dca66168d166f1ec6b86b
Nadira3 <paitanun35@gmail.com>
2024-01-12 07:47:13 +0100

    used correct apostrophes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'root'@'localhost'; --AND 'user_0d_2'@'host';
+SHOW GRANTS FOR "root'@'localhost"; --AND 'user_0d_2'@'host';
