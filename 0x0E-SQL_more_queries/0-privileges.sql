a05ad9beccdf8efaf4517423f378bd6d3dd0a485
Nadira3 <paitanun35@gmail.com>
2024-01-12 07:57:08 +0100

    handled error message

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 --AND 'user_0d_2'@'host'
-SHOW GRANTS FOR "user_0d_1'@'localhost";
-SHOW GRANTS FOR "user_0d_2'@'localhost";
-SHOW GRANTS FOR "root'@'localhost"; 
+
+
+SHOW GRANTS FOR "user_0d_1'@'localhost" IF EXISTS;
+SHOW GRANTS FOR "user_0d_2'@'localhost" IF EXISTS;
