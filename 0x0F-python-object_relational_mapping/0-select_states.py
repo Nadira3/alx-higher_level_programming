0e03a6499427e3734eb1ef86d70867fb715ef420
Nadira3 <paitanun35@gmail.com>
2024-01-13 19:18:00 +0100

    fixed pycodestyle error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-#Write a script that lists all states from the database hbtn_0e_0_usa:
+# Write a script that lists all states from the database hbtn_0e_0_usa:
 #
-#   Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+#   Your script should take 3 arguments: 
+#        - mysql username, 
+#        - mysql password and 
+#        - database name (no argument validation needed)
 #   You must use the module MySQLdb (import MySQLdb)
-#   Your script should connect to a MySQL server running on localhost at port 3306
+#   Your script should connect to a MySQL server running on
+#   localhost at port 3306
 #   Results must be sorted in ascending order by states.id
 #   Results must be displayed as they are in the example below
 #   Your code should not be executed when imported
@@ -15,9 +19,10 @@ if __name__ == '__main__':
     
     argv = sys.argv
     if len(argv) == 4:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],\
+                passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
             print(row)
