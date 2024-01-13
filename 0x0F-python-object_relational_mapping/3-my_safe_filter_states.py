536b0e0f1b4cc08f7227701957b6e3f2c8d64b4e
Nadira3 <paitanun35@gmail.com>
2024-01-13 20:47:36 +0100

    task 3 & 4 submission

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,41 @@
-#Write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
+#!/usr/bin/python3
+
+# Write a script that takes in an argument and displays
+# all values in the states table of hbtn_0e_0_usa where
+# name matches the argument.
 #
-#    Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)
+#    Your script should take 4 arguments:
+#    - mysql username, mysql password, database name and 
+#    - state name searched (no argument validation needed)
 #    You must use the module MySQLdb (import MySQLdb)
-#    Your script should connect to a MySQL server running on localhost at port 3306
+#    Script connects to MySQL server running on localhost@3306
+#    You must use format to create the SQL query with the user input
 #    Results must be sorted in ascending order by states.id
 #    Results must be displayed as they are in the example below
 #    Your code should not be executed when imported
+
+""" 
+    a module that lists all states starting 
+    from the database hbtn_0e_0_usa that matches
+    the argument given
+
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    argv = sys.argv
+    if len(argv) == 5:
+        conn = MySQLdb.connect(
+                host="localhost", port=3306, user=argv[1],
+                passwd=argv[2], db=argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute(
+                '''SELECT * FROM states WHERE name LIKE BINARY %s
+                ORDER BY id ASC''', (argv[4], ))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,6 @@
-#Write a script that lists all cities from the database hbtn_0e_4_usa
+#!/usr/bin/python3
+
+# Write a script that lists all cities from the database hbtn_0e_4_usa
 #
 #    Your script should take 3 arguments: mysql username, mysql password and database name
 #    You must use the module MySQLdb (import MySQLdb)
@@ -7,3 +9,24 @@
 #    You can use only execute() once
 #    Results must be displayed as they are in the example below
 #    Your code should not be executed when imported
+
+""" 
+    a module that lists all cities from the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    argv = sys.argv
+    if len(argv) == 5:
+        conn = MySQLdb.connect(
+                host="localhost", port=3306, user=argv[1],
+                passwd=argv[2], db=argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM cities ORDER BY id ASC')
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
