f91b3780937b923e4c99dfcd7726635127806411
Nadira3 <paitanun35@gmail.com>
2024-01-13 20:04:33 +0100

    tasks 1 & 2 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,11 +14,7 @@
 #    Results must be displayed as they are in the example below
 #    Your code should not be executed when imported
 
-""" 
-    a module that lists all states starting with
-    the letter N from the database hbtn_0e_0_usa 
-
-"""
+""" a module that lists all states from the database hbtn_0e_0_usa """
 
 if __name__ == '__main__':
     import sys
@@ -30,8 +26,9 @@ if __name__ == '__main__':
                 host="localhost", port=3306, user=argv[1],
                 passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states
-                    WHERE name LIKE 'N%' ORDER BY id ASC")
+        cur.execute(
+                "SELECT * FROM states WHERE name LIKE
+                'N%' ORDER BY id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
             print(row)
