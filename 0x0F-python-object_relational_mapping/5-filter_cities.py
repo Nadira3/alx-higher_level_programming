2a5dd180c2bac61e785ba7fc635fa2455797c005
Nadira3 <paitanun35@gmail.com>
2024-01-13 22:37:17 +0100

    task sith attempt

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,14 +20,14 @@ if __name__ == '__main__':
 
     argv = sys.argv
     if len(argv) == 5:
-        conn = MySQLdb.connect(
-                host="localhost", port=3306, user=argv[1],
-                passwd=argv[2], db=argv[3], charset="utf8")
+        #conn = MySQLdb.connect(
+         #       host="localhost", port=3306, user=argv[1],
+          #      passwd=argv[2], db=argv[3], charset="utf8")
+        conn = MySQLdb.connect(user=argv[2], db=argv[3])
         cur = conn.cursor()
-        cur.execute('''SELECT UNIQUE cities.name FROM cities 
+        cur.execute('''SELECT cities.name FROM cities 
                     JOIN states ON cities.state_id=states.id 
-                    WHERE states.name=%s 
-                    ORDER BY cities.id ASC''', (argv[4], ))
+                    WHERE states.name=%s''', (argv[4], ))
         query_rows = cur.fetchall()
         rows = list(row[0] for row in query_rows)
         print(*rows, sep=", ")
