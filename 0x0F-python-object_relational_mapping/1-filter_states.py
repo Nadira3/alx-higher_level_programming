f5daa3793f9bb896a4821a5bffd748a9364c636d
Nadira3 <paitanun35@gmail.com>
2024-01-13 19:48:06 +0100

    included shebang for execution

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,15 @@
-#Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+#!/usr/bin/python3
+
+# Write a script that lists all states with a name starting 
+# with N (upper N) from the database hbtn_0e_0_usa:
 #
-#    Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+#    Your script should take 3 arguments: 
+#        - mysql username,
+#        - mysql password and
+#        - database name (no argument validation needed)
 #    You must use the module MySQLdb (import MySQLdb)
-#    Your script should connect to a MySQL server running on localhost at port 3306
+#    Your script should connect to a MySQL server running on 
+#        localhost at port 3306
 #    Results must be sorted in ascending order by states.id
 #    Results must be displayed as they are in the example below
 #    Your code should not be executed when imported
@@ -23,7 +30,8 @@ if __name__ == '__main__':
                 host="localhost", port=3306, user=argv[1],
                 passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cur.execute("SELECT * FROM states
+                    WHERE name LIKE 'N%' ORDER BY id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
             print(row)
