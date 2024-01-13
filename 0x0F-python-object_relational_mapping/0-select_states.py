febe2b7497b6399bc55b35becf924529d8ac0d2b
Nadira3 <paitanun35@gmail.com>
2024-01-13 19:37:36 +0100

    finslly figured out the checker wasnt working because of missing module documentation. #sigh

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,9 @@
 
 # Write a script that lists all states from the database hbtn_0e_0_usa:
 #
-#   Your script should take 3 arguments: 
-#        - mysql username, 
-#        - mysql password and 
+#   Your script should take 3 arguments:
+#        - mysql username,
+#        - mysql password and
 #        - database name (no argument validation needed)
 #   You must use the module MySQLdb (import MySQLdb)
 #   Your script should connect to a MySQL server running on
@@ -13,13 +13,16 @@
 #   Results must be displayed as they are in the example below
 #   Your code should not be executed when imported
 
+""" a module that lists all states from the database hbtn_0e_0_usa """
+
 if __name__ == '__main__':
     import sys
     import MySQLdb
-    
+
     argv = sys.argv
     if len(argv) == 4:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],\
+        conn = MySQLdb.connect(
+                host="localhost", port=3306, user=argv[1],
                 passwd=argv[2], db=argv[3], charset="utf8")
         cur = conn.cursor()
         cur.execute("SELECT * FROM states ORDER BY id ASC")
