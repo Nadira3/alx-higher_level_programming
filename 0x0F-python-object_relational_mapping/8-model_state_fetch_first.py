2bc3bdc8f8a50815a342b03d4e17e4ec41eabcd8
Nadira3 <paitanun35@gmail.com>
2024-01-15 12:12:54 +0100

    pycodestyle error fix task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
-# Write a script that prints the first State object from the database hbtn_0e_6_usa
+# Write a script that prints the first State object
+#    from the database hbtn_0e_6_usa
 #
-#    Your script should take 3 arguments: mysql username, mysql password and database name
+#    Your script should take 3 arguments:
+#        - mysql username, mysql password and database name
 #    You must use the module SQLAlchemy
-#    You must import State and Base from model_state - from model_state import Base, State
-#    Your script should connect to a MySQL server running on localhost at port 3306
+#    You must import State and Bas
+#    Script should connect to a MySQL server running on localhost@3306
 #    The state you display must be the first in states.id
-#    You are not allowed to fetch all states from the database before displaying the result
+#    You are not allowed to fetch all states
+#        from the database before displaying the result
 #    The results must be displayed as they are in the example below
 #    If the table states is empty, print Nothing followed by a new line
 #    Your code should not be executed when imported
 
 
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -21,8 +24,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    #engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    engine = create_engine('mysql+mysqldb://{}@localhost/{}?'.format(sys.argv[1], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).order_by(State.id).first()
