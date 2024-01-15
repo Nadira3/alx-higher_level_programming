931bc000375df77dfb1bd2bf1306e6d7250bb3fe
Nadira3 <paitanun35@gmail.com>
2024-01-15 11:46:55 +0100

    task 8 submission

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,6 @@
-#Write a script that prints the first State object from the database hbtn_0e_6_usa
+#!/usr/bin/python3
+
+# Write a script that prints the first State object from the database hbtn_0e_6_usa
 #
 #    Your script should take 3 arguments: mysql username, mysql password and database name
 #    You must use the module SQLAlchemy
@@ -9,3 +11,22 @@
 #    The results must be displayed as they are in the example below
 #    If the table states is empty, print Nothing followed by a new line
 #    Your code should not be executed when imported
+
+
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        state = session.query(State).order_by(State.id)[0]
+        print("{}: {}".format(state.id, state.name))
+    except Exception:
+        ...
+    session.close()
