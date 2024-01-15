47b63d0d05538d46033cc538d35c431b0e341704
Nadira3 <paitanun35@gmail.com>
2024-01-15 16:51:12 +0100

    task 15 submission

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# Write a script 14-model_city_fetch_by_state.py that
+#    prints all City objects from the database hbtn_0e_14_usa:
+#
+#    Your script should take 3 arguments:
+#        - mysql username, mysql password and database name
+#    You must use the module SQLAlchemy
+#    You must import State and Base from model_state -
+#        from model_state import Base, State
+#    Script should connect to a MySQL server running on localhost@3306
+#    Results must be sorted in ascending order by cities.id
+#    Results must be display as they are in the example below
+#        (<state name>: (<city id>) <city name>)
+#    Your code should not be executed when imported
+
+
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state, city in session.query(State, City).\
+            filter(State.id == City.state_id).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Write a python file that contains the class definition of a State
+#   and an instance Base = declarative_base():
+#
+#    State class:
+#        inherits from Base Tips
+#        links to the MySQL table states
+#    class attribute id that represents a column of an auto-generated
+#        unique integer, can’t be null and is a primary key
+#    class attribute name that represents a column of a string
+#        with maximum 128 characters and can’t be null
+#    You must use the module SQLAlchemy
+
+""" module that defines Base and State """
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_city import City
+
+Base = declarative_base()
+
+class State(Base):
+    """ State class definition """
+    __tablename__ = 'states'
+    ...
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128))
+
+    cities = relationship("City", back_populates="state")
