eada8e3e48ca501bc4dabed7855d5432f4142665
Nadira3 <paitanun35@gmail.com>
2024-01-15 14:13:07 +0100

    task 13 submission

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,34 @@
-#Write a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
-#
-#    Your script should take 3 arguments: mysql username, mysql password and database name
+#!/usr/bin/python3
+
+# Write a script that deletes all State objects with a name
+#        containing the letter a from the database hbtn_0e_6_usa
+#    Your script should take 3 arguments:
+#        - mysql username, mysql password and database name
 #    You must use the module SQLAlchemy
-#    You must import State and Base from model_state - from model_state import Base, State
-#    Your script should connect to a MySQL server running on localhost at port 3306
+#    You must import State and Base from model_state
+#        - from model_state import Base, State
+#    Script should connect to a MySQL server running on localhost@3306
+#    Results must be sorted in ascending order by states.id
+#    The results must be displayed as they are in the example below
 #    Your code should not be executed when imported
+
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    #connext and create a session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #find the matching objects and delete
+    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')).all(): 
+        session.delete(state)
+
+    # commit the changes
+    session.commit()
+    session.close()
