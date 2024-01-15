26a34379b71942196741bfb3b2470ec9982d3b78
Nadira3 <paitanun35@gmail.com>
2024-01-15 13:49:00 +0100

    task 11 submissiom

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,6 @@
-#Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+#!/usr/bin/python3
+
+# Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 #
 #    Your script should take 3 arguments: mysql username, mysql password and database name
 #    You must use the module SQLAlchemy
@@ -6,3 +8,21 @@
 #    Your script should connect to a MySQL server running on localhost at port 3306
 #    Print the new states.id after creation
 #    Your code should not be executed when imported
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    session.add(State(name='Louisiana'))
+    
+    state = session.query(State).order_by(State.id).all()
+    print(state[-1].id)
+
+    session.commit()
+    session.close()
