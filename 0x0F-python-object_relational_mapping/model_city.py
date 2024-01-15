730704929655b368c10f23bda49fb3215730481b
Nadira3 <paitanun35@gmail.com>
2024-01-15 15:39:19 +0100

    pycodestyle validation fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
-# Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City.
-#    
+# Write a Python file similar to model_state.py named
+#    model_city.py that contains the class definition of a City.
+#
 #    City class:
 #        inherits from Base (imported from model_state)
 #        links to the MySQL table cities
-#    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-#    class attribute name that represents a column of a string of 128 characters and can’t be null
-#    class attribute state_id that represents a column of an integer, can’t be null and is a foreign key to states.id
+#    class attribute id that represents a column of an auto-generated,
+#        unique integer, can’t be null and is a primary key
+#    class attribute name that represents a
+#        column of a string of 128 characters and can’t be null
+#    class attribute state_id that represents a column of an integer,
+#        can’t be null and is a foreign key to states.id
 #    You must use the module SQLAlchemy
 
-""" 
+"""
     module that contains the class definition of a State
     and an instance Base = declarative_base()
 """
@@ -20,14 +24,12 @@ from sqlalchemy.orm import relationship
 from model_state import State, Base
 
 
-argv = sys.argv
-
 class City(Base):
     """ City class definition """
     __tablename__ = 'cities'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128))
     state_id = Column(Integer, ForeignKey("states.id"))
-    
+
     state = relationship("State", back_populates="cities")
