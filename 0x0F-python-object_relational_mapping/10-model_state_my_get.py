3ac08688e4dc13b1d09100104bc4142e5f1328a2
Nadira3 <paitanun35@gmail.com>
2024-01-15 13:17:54 +0100

    tasl 10 correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,6 +23,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name == '%s' % sys.argv[4]).order_by(State.id):
-        print("{}".format(state.id) if state else "Not found")
+    states = session.query(State).filter(State.name == '%s' % sys.argv[4]).order_by(State.id)
+    if states:
+        for state in states.all():
+            print(state.id)
+    else:
+        print("Not found")
     session.close()
