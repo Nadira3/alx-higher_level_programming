9881fba4e6e6d82ca6a0e0c59b1d5e6f45441a05
Nadira3 <paitanun35@gmail.com>
2024-01-15 14:17:14 +0100

    module is documented

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,6 +14,8 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+""" module that adds an object """
+
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
