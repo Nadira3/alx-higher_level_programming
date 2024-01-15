8af3a346d97d36b0041c6de2f90b732a2f5167e5
Nadira3 <paitanun35@gmail.com>
2024-01-15 07:24:53 +0100

    model state first submission

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,21 @@
-#Write a python file that contains the class definition of a State and an instance Base = declarative_base():
+#!/usr/bin/python3
+
+# Write a python file that contains the class definition of a State 
+#   and an instance Base = declarative_base():
 #
 #    State class:
 #        inherits from Base Tips
 #        links to the MySQL table states
-#    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-#    class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+#    class attribute id that represents a column of an auto-generated
+#        unique integer, can’t be null and is a primary key
+#    class attribute name that represents a column of a string
+#        with maximum 128 characters and can’t be null
 #    You must use the module SQLAlchemy
-#    Your script should connect to a MySQL server running on localhost at port 3306
-#    WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine)
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+    print("{}: {}".format(state.id, state.name))
+session.close()
