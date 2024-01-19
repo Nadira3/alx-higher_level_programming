775a6372b2a57760606043e44b59e22aca731ba7
Nadira3 <paitanun35@gmail.com>
2024-01-19 05:27:28 +0100

    pycodestyle error fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,12 +18,12 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
+
 Base = declarative_base()
 
 class State(Base):
     """ State class definition """
     __tablename__ = 'states'
-    ...
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
