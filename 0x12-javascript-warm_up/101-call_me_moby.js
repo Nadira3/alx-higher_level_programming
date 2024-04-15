a39e77a227b5f34facd1700f374a272e8d0a1f36
Nadira3 <paitanun35@gmail.com>
2024-04-15 12:54:23 +0100

    feat => fixed error: Missing space before function parentheses. (space-before-function-paren)

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-    }
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 }
 
 // Export the function so that it can be used in other modules/files
 module.exports = {
-    callMeMoby: callMeMoby
+  callMeMoby
 };
-
--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 
 // Define the incr function
 myObject.incr = function () {
-    this.value += 1; // Increment the value property by 1
+  this.value += 1; // Increment the value property by 1
 };
 
 // Call the incr function and log the updated object
@@ -19,4 +19,3 @@ console.log(myObject);
 
 myObject.incr();
 console.log(myObject);
-
